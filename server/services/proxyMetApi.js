6f30bdb95d76bd75060c8baf561e6635986006ec
Lukas Mahler <lukas.mah@outlook.at>
2021-05-06 21:27:09 +0200

    move api calls from artworks to proxyMetApi

--- server/services/proxyMetApi.js
+++ server/services/proxyMetApi.js
@@ -1,49 +1,69 @@
-import * as ArtworkCache from 'artworkCache.js';
-import * as SearchCache from 'searchCache.js';
-
 const fetch = require('node-fetch');
+const express = require('express');
+const routes = express.Router();
+const fs = require('fs');
+const path = require('path');
+const cache = require('node-cache');
 
-/**
- * return an ArtworkObject for the given Id
- * 
- * @param {*} id of the artwork. 
- * @returns the object for the given id.
- */
- export function getArtworkObject(id){
+const highlights = require('../resources/highlights.json');
+const Artwork = require('../models/artwork.js');
 
-    // let artwork = ArtworkCache.retrieve(id);
-    // if (artwork) return artwork;
+const MET_BASE_URL = 'https://collectionapi.metmuseum.org/public/collection/v1';
 
-    return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`, {
-        method: 'GET',
-        // body: myBody,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then( response => response = response.json())
-    .then(response => {return response.message == null? response : null});
+async function getArtwork(id) {
+    console.log(`call getArtwork(${id})`);
+    console.log(`fetching ${MET_BASE_URL + '/objects/' + id}`);
+    const res = await fetch(MET_BASE_URL + '/objects/' + id);
+  
+    if (!res.ok) {
+      console.log("response from met-api not ok");
+      return null;
+    }
+    const obj = await res.json();
+    if (!obj || !obj.objectID) {
+      console.log("response from met-api not an object");
+      return null;
+    }
+  
+    console.log("creating artwork for return");
+    // TODO: convert Met object - done?
+    let artwork = new Artwork (
+      obj.objectID,
+      obj.title,
+      obj.artistDisplayName,
+      obj.objectDate,
+      obj.primaryImageSmall
+    )
+  
+    console.log(`artwork: ${JSON.stringify(artwork)}`);
+    return artwork;
+}
+  
+async function getSearch(searchParam) {
+    console.log(`call getSearch(${searchParam})`);
+    console.log(`fetch via ${MET_BASE_URL + '/search?q=' + searchParam}`);
+    const res = await fetch(MET_BASE_URL + '/search?q=' + searchParam);
+
+    if (!res.ok) {
+        return null;
+    }
+
+    const obj = await res.json();
+    console.log(obj);
+
+    if(!obj) {
+        console.log("no obj or objID");
+        return null;
+    }
+
+    console.log(obj);
+
+    console.log('returning objectIds: ')
+    console.log(obj.objectIDs);
+
+    return obj.objectIDs;
+
 }
 
-/**
- * return an array of all objectIds (of artworks) for the given search parameters.
- * 
- * @param {*} searchParam the string by which it is searched.
- * @param {*} hasImages exclude artworks without images.
- * @returns the array of all objectIds
- */
-export function getArtworkSearch(searchParam, hasImages){
-
-    // let search = SearchCache.retrieve(searchParam);
-    // if (search) return search;
-
-    return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchParam}&hasImages=${hasImages}`, {
-        method: 'GET',
-        // body: myBody,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then( response => response = response.json())
-    .then( response => response = response.objectIDs);
-}
+exports.getArtwork = getArtwork;
+exports.getSearch = getSearch;
