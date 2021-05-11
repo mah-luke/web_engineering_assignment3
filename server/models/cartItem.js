1c0cbed51588dd1c321a46005b14d9b9ec2d2d7c
Lukas Mahler <lukas.mah@outlook.at>
2021-05-11 20:46:25 +0200

    add cartItem

--- server/models/cartItem.js
+++ server/models/cartItem.js
@@ -1,6 +1,13 @@
 class CartItem {
-    constructor() {
-        
+    constructor(cartItemId, price, artworkId, printSize, frameStyle, frameWidth, matColor, matWidth) {
+        this.cartItemId = cartItemId;
+        this.price = price;
+        this.artworkId = artworkId;
+        this.printSize = printSize;
+        this.frameStyle = frameStyle;
+        this.frameWidth = frameWidth;
+        this.matColor = matColor;
+        this.matWidth = matWidth;
     }
 }
 
