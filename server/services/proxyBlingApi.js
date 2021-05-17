821ceef1595d89e82789b2a386cfc7b78bc26268
DonikaSkenderbeut <e11929150@student.tuwien.ac.at>
2021-05-17 14:06:03 +0200

    environment variables added

--- server/services/proxyBlingApi.js
+++ server/services/proxyBlingApi.js
@@ -1,22 +1,22 @@
 const fetch = require('node-fetch');
+require('dotenv').config();
 
 const PaymentIntent = require('../models/PaymentIntent.js');
 const BLING_BASE_URL = 'https://web-engineering.big.tuwien.ac.at/s21/bling';
-const WEBHOOK = "https://localhost:3000/cart/checkout/payment-update";
-const BLING_API_KEY = Buffer.from('ak_s21a3g085' + ':').toString('base64');
+//const BLING_API_KEY = Buffer.from('ak_s21a3g085' + ':').toString('base64');
 
 async function postPaymentIntent(price) {
     let data = JSON.stringify({
         amount: price,
         currency: "eur",
-        webhook: WEBHOOK
+        webhook: process.env.WEBHOOK
     });
 
     const options = {
         method: 'POST',
         body: data,
         headers: {
-            "Authorization": 'Basic ' + BLING_API_KEY, 
+            "Authorization": 'Basic ' + Buffer.from(process.env.BLING_API_KEY + ':').toString('base64'), 
             "Content-Type": 'application/json',
             "Content-Length": data.length
         }
